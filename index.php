1b2bbb8c6121c3e9642e0e5bc75822fb6009084c
salkrid <bohner@yandex.ru>
2020-03-25 10:34:07 +0300

    Сделана оболочка

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <div class="container">
       <div class = "row coffee-machine">
         <div class = "col-6 coffee-list"><!--это коммент-->
-          <div class="row flex-column p-3">
+          <div class="row flex-column p-3 h-100 justify-content-around">
             <div class="coffee-item col">
               <img src="img/americano.png" alt="">
               <span>Американо - 50 руб.</span>
@@ -43,8 +43,23 @@
                   <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
                 </div>
               </div>
+              <div class="coffee-cup">
+                <img src="img/americano.png" alt="">
+              </div>
             </div>
-            <div class="col-6"></div>
+            <div class="col-6">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Баланс" aria-label="Имя получателя" aria-describedby="basic-addon2"> <!--aria-label="Имя получателя" aria-describedby="basic-addon2" необ элементы-->
+                <div class="input-group-append">
+                  <span class="input-group-text" id="basic-addon2">&#8381</span>
+                </div>
+              </div>
+              <div class="atm">
+                <img src="img/bill_acc.png" alt="">
+              </div>
+              <button class="btn btn-primary btn-block mt-2">Сдача</button> <!--тег button html-->
+              <div class="change-box"></div>
+            </div>
           </div>  
         </div>
       </div>
