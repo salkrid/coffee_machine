2ee628a2bc569161f6df1849a9ca2a10610af9da
salkrid <bohner@yandex.ru>
2020-03-26 13:29:01 +0300

    Научили готовить кофе

--- index.php
+++ index.php
@@ -16,19 +16,19 @@
       <div class = "row coffee-machine">
         <div class = "col-6 coffee-list"><!--это коммент-->
           <div class="row flex-column p-3 h-100 justify-content-around">
-            <div class="coffee-item col" onclick="buyCoffee('Американо', 50)">
+            <div class="coffee-item col" onclick="buyCoffee('Американо', 50, this)">
               <img src="img/americano.png" alt="">
               <span>Американо - 50 руб.</span>
             </div>            
-            <div class="coffee-item col" onclick="buyCoffee('Капучино', 78)">
+            <div class="coffee-item col" onclick="buyCoffee('Капучино', 78, this)">
               <img src="img/cappuccino.png" alt="">
               <span>Капучино - 78 руб.</span>
             </div>            
-            <div class="coffee-item col" onclick="buyCoffee('Эспрессо', 21)">
+            <div class="coffee-item col" onclick="buyCoffee('Эспрессо', 21, this)">
               <img src="img/espresso.png" alt="">
               <span>Эспрессо - 21 руб.</span>
             </div>            
-            <div class="coffee-item col" onclick="buyCoffee('Латте', 115)">
+            <div class="coffee-item col" onclick="buyCoffee('Латте', 115, this)">
               <img src="img/latte.jpg" alt="">
               <span>Латте - 115 руб.</span>
             </div>
@@ -40,11 +40,11 @@
               <div class="display">
                 <p class="display-text">Выберите кофе</p>
                 <div class="progress">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
                 </div>
               </div>
               <div class="coffee-cup">
-                <img src="img/americano.png" alt="">
+                <img class="d-none" src="img/americano.png" alt="">
               </div>
             </div>
             <div class="col-6">
--- script.js
+++ script.js
@@ -1,14 +1,68 @@
 "use strict" 
 /*не позволяет объявлять переменные без использования let*/
 let balance = document.querySelector(".balance");
+let displayText = document.querySelector(".display-text");
+let progressBar = document.querySelector(".progress-bar");
+let coffeeCup = document.querySelector(".coffee-cup img");
 
-function buyCoffee(name, cost) {
+let coffeeStatus = "waiting"; // "cooking" "ready" - ожидание заказа
+
+
+function buyCoffee(name, cost, elem) {
+  if(coffeeStatus != "waiting") {
+    return;
+  }
   let afterBuyValue = +balance.value - cost;
  /* alert(`Вы заказали ${name}. Цена: ${cost}`);*/
   if ( (balance.value - cost) < 0 || Number.isNaN(afterBuyValue)) {// Возвращает значение без "="
-    alert("Недостаточно средств!");
+    balance.style.border = "2px solid red";
+    balance.style.backgroundColor = "pink";
+    changeDisplayText("Недостаточно средств");
+   /* alert("Недостаточно средств!");*/
     return;
   }
+  balance.style.border = "none";
+  balance.style.background = "white";
   balance.value = (+balance.value - cost).toFixed(2);
-  alert("Ваш " + name + " готовится!");
+  cookCoffee(name, elem);
+  // alert("Ваш " + name + " готовится!");
+}
+
+function cookCoffee(name, elem) {
+  coffeeStatus = "cooking";
+  changeDisplayText("Ваш " + name + " готовится");
+  
+  let cupImg = elem.querySelector("img");
+  let cupSrc = cupImg.getAttribute("src");
+  coffeeCup.setAttribute("src", cupSrc); // какой атрибут меня, на что его меняем
+ // console.log(cupImg);
+  coffeeCup.style.opacity = "0%";
+  coffeeCup.classList.remove("d-none");
+ 
+ //coffeCup.classList.add(""); // list всех классов кот. там есть, add добавить класс
+  //coffeCup.classList.remove(""); //убрать класс
+  //coffeCup.classList.toggle(""); // включить/выключить класс
+  
+ //coffeCup.classList.contains("d-none"); // Содержит ли?
+  
+  let readyPercent = 0; // насколько % кофе приготовился
+  let cookingInterval = setInterval(() => {
+    readyPercent++
+    progressBar.style.width = readyPercent + "%";
+    coffeeCup.style.opacity = readyPercent + "%";
+    if(readyPercent == 100) {
+      coffeeStatus = "ready";
+      changeDisplayText("Ваш " + name + " готов!");
+      coffeeCup.style.cursor = "pointer";
+      clearInterval(cookingInterval);
+    }
+  }, 100);
+  
+//  console.log(elem);
+}
+
+function changeDisplayText(text) {
+  // displayText.innerText = "<span>"+text+"</span>";
+  displayText.innerHTML = "<span>"+text+"</span>";
+  
 }
